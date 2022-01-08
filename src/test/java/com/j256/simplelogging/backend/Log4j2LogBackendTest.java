e18debfe56796da584f9ab18f6f4ec62cc70e119
Gray Watson <gray.github@mailgw.com>
2022-01-08 12:01:15 -0500

    Improved coverage.

--- src/test/java/com/j256/simplelogging/backend/Log4j2LogBackendTest.java
+++ src/test/java/com/j256/simplelogging/backend/Log4j2LogBackendTest.java
@@ -1,10 +1,7 @@
 package com.j256.simplelogging.backend;
 
-import org.junit.Ignore;
-
 import com.j256.simplelogging.backend.Log4j2LogBackend.Log4j2LogBackendFactory;
 
-@Ignore("won't work if we are running under java < 8")
 public class Log4j2LogBackendTest extends BaseLogBackendTest {
 
 	public Log4j2LogBackendTest() {
---
+++ src/test/java/com/j256/simplelogging/backend/LogBackendUtilTest.java
@@ -0,0 +1,11 @@
+package com.j256.simplelogging.backend;
+
+import org.junit.Test;
+
+public class LogBackendUtilTest {
+
+	@Test
+	public void testCoverage() {
+		new LogBackendUtil();
+	}
+}
