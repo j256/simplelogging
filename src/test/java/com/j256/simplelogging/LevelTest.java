1a524bba1f84a239ad858526dac7051a0c61da5a
Gray <j256@users.noreply.github.com>
2022-01-09 14:19:35 -0500

    Improved coverage (#18)

---
+++ src/test/java/com/j256/simplelogging/LevelTest.java
@@ -0,0 +1,16 @@
+package com.j256.simplelogging;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LevelTest {
+
+	@Test
+	public void testCoverage() {
+		assertFalse(Level.OFF.isEnabled(Level.TRACE));
+		assertFalse(Level.TRACE.isEnabled(Level.OFF));
+		assertTrue(Level.TRACE.isEnabled(Level.INFO));
+		assertFalse(Level.INFO.isEnabled(Level.TRACE));
+	}
+}
